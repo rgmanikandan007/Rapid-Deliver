83f237e3ff4baf6e96e1c00f96ea24dcfae9fe67
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-12-29 14:18:14 +0530

    rapid

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navlogo from ''
+
+function Navbar() {
+  return (
+    <div className='navbar-container'>
+        <div className='navlogo'>
+            <img src={Navlogo} />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

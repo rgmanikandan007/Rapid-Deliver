12490d682138571a48dc0297e5eb30c18e1c4b4f
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-12-29 15:15:09 +0530

    rapid

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import Navlogo from ''
+import Navlogo from '../Images/Navlogo-copy.jpg'
+import { MdAddShoppingCart } from "react-icons/md";
 
 function Navbar() {
   return (
     <div className='navbar-container'>
+      <div className='top-navbar'>
         <div className='navlogo'>
-            <img src={Navlogo} />
+          <img src={Navlogo} />
         </div>
+
+        <div className='search-box'>
+          <input type='text' placeholder='Search Product' />
+        </div>
+
+        <div className='Addcart-icon'>
+          <h2><MdAddShoppingCart /></h2>
+        </div>
+      </div>
     </div>
   )
 }

643ef180be4422e6ed1548e19804755fd0be2ccb
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-12-31 12:24:07 +0530

    login

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <div className='footer-left'>
+            <div className='footer-left-child'></div>
+            <div className='footer-left-child'></div>
+            <div className='footer-left-child'></div>
+        </div>
+        <div className='footer-right'></div>
+    </div>
+  )
+}
+
+export default Footer

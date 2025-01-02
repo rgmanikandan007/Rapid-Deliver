970b1e6bed3d4ffed9c6f9d88376ac8dab0c6541
rgmanikandan07 <rgmanikandan07@gmail.com>
2025-01-02 12:28:54 +0530

    rapid

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import BannerImg from '../Images/banner.jpg'
+import subBanner1 from '../Images/sub-banner (1).jpg'
+import subBanner2 from '../Images/sub-banner (2).jpg'
+import subBanner3 from '../Images/sub-banner (3).jpg'
 
 function Banner() {
   return (
-    <div className='banner-container'>
-        <img src={BannerImg} alt='Banner-img'/>
-    </div>
+    <>
+      <div className='banner-container'>
+        <img src={BannerImg} alt='Banner-img' />
+      </div>
+
+      <div className='sub-banner'>
+        <img src={subBanner1} />
+        <img src={subBanner2} />
+        <img src={subBanner3} />
+      </div>
+    </>
   )
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import Navlogo from '../Images/Navlogo-copy.jpg'
+import Navlogo from '../Images/Navlogo-copy2.png'
 import { MdAddShoppingCart } from "react-icons/md";
 import { TbLogin2 } from "react-icons/tb";
 
+import Tooltip from '@mui/material/Tooltip';
+
 function Navbar() {
+
+
   return (
     <div className='navbar-container'>
       <div className='top-navbar'>
         <div className='navlogo'>
-          <img src={Navlogo} />     
-        </div>  
+          <img src={Navlogo} />
+        </div>
 
         <div className='deliver-time'>
           <h3>Delivery time in 9 minutes</h3>
@@ -21,7 +25,9 @@ function Navbar() {
         </div>
 
         <div className='Addcart-icon'>
-          <h2><TbLogin2 /></h2> 
+          <Tooltip title="Login" placement="left-start">
+            <h2><TbLogin2 /></h2>
+          </Tooltip>
           <h2><MdAddShoppingCart /></h2>
         </div>
       </div>
